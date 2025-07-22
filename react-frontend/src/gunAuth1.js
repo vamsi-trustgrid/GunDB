3f2f98358f4e26fee6f79189b418f25865e7f271
uv0107 <uyyalavamsi37@gmail.com>
2025-07-22 17:16:58 +0530

    Add gunAuth1.js

--- react-frontend/src/gunAuth1.js
+++ react-frontend/src/gunAuth1.js
@@ -1,5 +1,5 @@
 import react from "react";
 import Gun from 'gun';
 import 'gun/sea';
-export const gun = Gun({ peers: ['https://0a307f264fb8.ngrok-free.app/gun'] });
+export const gun = Gun({ peers: ['https://8ff56593cc27.ngrok-free.app '] });
 export const user = gun.user();
