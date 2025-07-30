3daed7d733deec7e16b125c1574663aa9f38a137
uv0107 <uyyalavamsi37@gmail.com>
2025-07-30 14:07:02 +0530

    ngrok is updated

--- react-frontend/src/gunAuth1.js
+++ react-frontend/src/gunAuth1.js
@@ -1,5 +1,5 @@
 import react from "react";
 import Gun from 'gun';
 import 'gun/sea';
-export const gun = Gun({ peers: ['https://666b6ab550f8.ngrok-free.app/gun'] });
+export const gun = Gun({ peers: ['https://d4dd27cecb35.ngrok-free.app /gun'] });
 export const user = gun.user();
